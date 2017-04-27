a7ea5428e7202befd609846e56ff9dc990f5b5a8
anLA7856 <350493391@qq.com>
2017-04-27 16:19:32 +0800

    新增一个参数检测类，基本将接收新消息的servlet写完
    Signed-off-by: anLA7856<350493391@qq.com>

--- src/csust/sign/stuServlet/StuChatMessageAddServlet.java
+++ src/csust/sign/stuServlet/StuChatMessageAddServlet.java
@@ -11,8 +11,11 @@ import javax.servlet.http.HttpServletResponse;
 
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
+import csust.sign.bean.ChatMessage;
 import csust.sign.bean.StudentInfo;
+import csust.sign.bean.Dao.Impl.ChatMessageDaoImpl;
 import csust.sign.bean.Dao.Impl.StudentDaoImpl;
+import csust.sign.utils.ParameterUtil;
 
 public class StuChatMessageAddServlet extends HttpServlet {
 
@@ -21,7 +24,7 @@ public class StuChatMessageAddServlet extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	private StudentDaoImpl sdi = new StudentDaoImpl();
+	private ChatMessageDaoImpl cmdi = new ChatMessageDaoImpl();
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -35,44 +38,23 @@ public class StuChatMessageAddServlet extends HttpServlet {
 		// 后期必须采用加密策略
 		resp.setContentType("text/html;charset=utf-8");
 		PrintWriter pw = resp.getWriter();
-		String value = req.getParameter("value");
+		String studentId = req.getParameter("studentId");
+		String teacherId = req.getParameter("teacherId");
+		String message = req.getParameter("message");
 		
-		if(value == null){
+		if(!ParameterUtil.parameterTest(studentId,teacherId,message)){
 			return;
 		}
+		ChatMessage cm = new ChatMessage();
+		cm.setSenderId(Integer.parseInt(studentId.toString()));
+		cm.setReceiveId(Integer.parseInt(teacherId.toString()));
+		cm.setMessage(message.toString());
+		//保存。
+		
+		int validate = cmdi.addNewMessage(cm);
+		pw.print(validate);
+		pw.flush();
+		pw.close();
 
-		if (!value.equalsIgnoreCase("")) {
-
-			JSONObject jsonObject1 = JSONObject.fromObject(value);
-			String username = jsonObject1.get("username").toString().trim();
-			String password = jsonObject1.get("password").toString().trim();
-			String name1 = jsonObject1.get("name").toString().trim();
-			String name = new String(name1.getBytes("ISO-8859-1"), "UTF-8");
-			String sex1 = jsonObject1.get("sex").toString().trim();
-			String sex = new String(sex1.getBytes("ISO-8859-1"), "UTF-8");
-			String age = jsonObject1.get("age").toString().trim();
-			String stuNum = jsonObject1.get("stuNum").toString().trim();
-			StudentInfo student = new StudentInfo(0, name, sex, stuNum,
-					username, password);
-			int validate = sdi.validataUsername(username);
-			if (validate == 1) {
-				// 表示该用户名已经被注册了，不能注册了
-				pw.print("no");
-			} else {
-				// 可以注册
-				int result = sdi.addStudent(student);
-				if (result == 1) {
-					// 注册成功！
-					pw.print("ok");
-				} else {
-					// 不明原因注册失败
-					pw.print("fail");
-				}
-			}
-
-			pw.flush();
-			pw.close();
-
-		}
 	}
 }
---
+++ src/csust/sign/utils/ParameterUtil.java
@@ -0,0 +1,20 @@
+package csust.sign.utils;
+
+/**
+ * 用于验证参数。
+ * @author U-ANLA
+ *
+ */
+public class ParameterUtil {
+	
+	public static boolean parameterTest(String... params){
+		//首先验证是否为空，如果有一个为null就返回false,具有短路功能。
+		for(int i = 0;i < params.length;i++){
+			if(params[i] == null || params.equals("")){
+				return false;
+			}
+		}
+		
+		return true;
+	}
+}
