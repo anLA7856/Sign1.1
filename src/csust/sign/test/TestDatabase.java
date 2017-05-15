586acb7d2b8a0fa89acd100674d770a99cbf84a5
anLA7856 <350493391@qq.com>
2017-05-15 13:18:32 +0800

    test
    Signed-off-by: anLA7856<350493391@qq.com>

--- src/csust/sign/test/TestDatabase.java
+++ src/csust/sign/test/TestDatabase.java
@@ -2,14 +2,17 @@ package csust.sign.test;
 
 import csust.sign.bean.Dao.Impl.CourseDaoImpl;
 
-
-
+/**
+ * 
+ * @author anLA7856
+ *
+ */
 public class TestDatabase {
 	public static void main(String[] args) {
 		final CourseDaoImpl c = new CourseDaoImpl();
 		final CourseDaoImpl c1 = new CourseDaoImpl();
 		final CourseDaoImpl c2 = new CourseDaoImpl();
 		final CourseDaoImpl c3 = new CourseDaoImpl();
-		
+
 	}
 }
